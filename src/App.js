d8970bc3a0328c30333f3834333ac079dc6fccce
asrafulislamgh <gdmamun@yahoo.com>
2021-11-13 21:49:37 +0600

    revised

--- src/App.js
+++ src/App.js
@@ -2,14 +2,9 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./App.css";
 import AuthProvider from "./contexts/AuthProvider";
-import AdminRoute from "./Pages/AdminRoute/AdminRoute";
 import Dashboard from "./Pages/Dashboard/Dashboard";
 import Home from "./Pages/Home/Home";
-import Review from "./Pages/Home/Review/Review";
 import Login from "./Pages/login/Login";
-import MakeAdmin from "./Pages/MakeAdmin/MakeAdmin";
-import ManageOrders from "./Pages/ManageOrders/ManageOrders";
-import MyOrder from "./Pages/MyOrder/MyOrder";
 import PrivateRoute from "./Pages/PrivateRoute/PrivateRoute";
 import Properties from "./Pages/Properties/Properties";
 import PropertyDetail from "./Pages/Property/PropertyDetail";
@@ -41,7 +36,7 @@ function App() {
           <PrivateRoute path="/properties/:id">
             <PropertyDetail />
           </PrivateRoute>
-          <PrivateRoute path="/myorder">
+          {/* <PrivateRoute path="/myorder">
             <MyOrder />
           </PrivateRoute>
           <PrivateRoute path="/review">
@@ -52,7 +47,7 @@ function App() {
           </AdminRoute>
           <AdminRoute path="/makeadmin">
             <MakeAdmin />
-          </AdminRoute>
+          </AdminRoute> */}
         </Switch>
       </Router>
     </AuthProvider>
--- src/Pages/Property/PropertyDetail.js
+++ src/Pages/Property/PropertyDetail.js
@@ -13,6 +13,8 @@ import {
 import { useParams, Link, useHistory } from "react-router-dom";
 import useAuth from "../../hooks/useAuth";
 import Swal from "sweetalert2";
+import HeaderNav from "../Shared/HeaderNav/HeaderNav";
+import Footer from "../Shared/Footer/Footer";
 // import "./Appointment.css";
 
 const PropertyDetail = () => {
@@ -53,97 +55,105 @@ const PropertyDetail = () => {
     })
       .then((res) => res.json())
       .then((result) => {
-        history.push("/myorder");
+        history.push("/dashboard");
         Swal.fire("Good job!", "You have ordered successfully!", "success");
       });
     reset();
     // };
   };
   return (
-    <div className="my-5 py-5">
-      <Row className="gy-3 gy-sm-5">
-        <Col xl={5} lg={4} md={6}>
-          <div>
-            <img className="w-100 rounded" src={selectedProperty?.img} alt="" />
-          </div>
-        </Col>
-        <Col xl={3} lg={3} md={5} className="mx-4">
-          <div>
-            <h1>{selectedProperty?.name}</h1>
-            <p>{selectedProperty?.description}</p>
-            <Card.Text className="card-info">
-              <Badge pill bg="warning" className="px-3 me-2" text="dark">
-                <h6 className="mb-0"> Day: {selectedProperty?.day}</h6>
-              </Badge>
-              <Badge pill bg="dark" className="px-3  me-2" text="light">
-                <h6 className="mb-0"> Day: {selectedProperty?.night}</h6>
-              </Badge>
-            </Card.Text>
-            <h3>${selectedProperty?.price}</h3>
-          </div>
-        </Col>
-        <Col xl={3} lg={3} md={12} className="mx-4">
-          <Row className="d-flex justify-content-center text-center">
-            <Col style={{ maxWidth: "400px" }}>
-              <h3 className="py-3">Order Now</h3>
-              <Form onSubmit={handleSubmit(onSubmit)}>
-                <FormControl
-                  {...register("displayName")}
-                  className="input-field mb-3"
-                  placeholder="Name"
-                  aria-label="name"
-                  value={user.displayName}
-                  defaultValues={user.displayName}
-                  aria-describedby="basic-addon2"
-                />
+    <div>
+      <HeaderNav />
+      <div className="my-5 py-5">
+        <Row className="gy-3 gy-sm-5">
+          <Col xl={5} lg={4} md={6}>
+            <div>
+              <img
+                className="w-100 rounded"
+                src={selectedProperty?.img}
+                alt=""
+              />
+            </div>
+          </Col>
+          <Col xl={3} lg={3} md={5} className="mx-4">
+            <div>
+              <h1>{selectedProperty?.name}</h1>
+              <p>{selectedProperty?.description}</p>
+              <Card.Text className="card-info">
+                <Badge pill bg="warning" className="px-3 me-2" text="dark">
+                  <h6 className="mb-0"> Day: {selectedProperty?.day}</h6>
+                </Badge>
+                <Badge pill bg="dark" className="px-3  me-2" text="light">
+                  <h6 className="mb-0"> Day: {selectedProperty?.night}</h6>
+                </Badge>
+              </Card.Text>
+              <h3>${selectedProperty?.price}</h3>
+            </div>
+          </Col>
+          <Col xl={3} lg={3} md={12} className="mx-4">
+            <Row className="d-flex justify-content-center text-center">
+              <Col style={{ maxWidth: "400px" }}>
+                <h3 className="py-3">Order Now</h3>
+                <Form onSubmit={handleSubmit(onSubmit)}>
+                  <FormControl
+                    {...register("displayName")}
+                    className="input-field mb-3"
+                    placeholder="Name"
+                    aria-label="name"
+                    value={user.displayName}
+                    defaultValues={user.displayName}
+                    aria-describedby="basic-addon2"
+                  />
 
-                <FormControl
-                  {...register("email")}
-                  className="input-field mb-3"
-                  placeholder="Email"
-                  aria-label="email"
-                  value={user.email}
-                  defaultValues={user.email}
-                  type="email"
-                  aria-describedby="basic-addon2"
-                />
-                <FormControl
-                  {...register("phone", { required: true })}
-                  className="input-field mb-3"
-                  placeholder="Phone"
-                  aria-label="phone"
-                  aria-describedby="basic-addon2"
-                />
-                <FormControl
-                  {...register("address", { required: true })}
-                  className="input-field mb-3"
-                  placeholder="Address"
-                  aria-label="address"
-                  aria-describedby="basic-addon2"
-                />
+                  <FormControl
+                    {...register("email")}
+                    className="input-field mb-3"
+                    placeholder="Email"
+                    aria-label="email"
+                    value={user.email}
+                    defaultValues={user.email}
+                    type="email"
+                    aria-describedby="basic-addon2"
+                  />
+                  <FormControl
+                    {...register("phone", { required: true })}
+                    className="input-field mb-3"
+                    placeholder="Phone"
+                    aria-label="phone"
+                    aria-describedby="basic-addon2"
+                  />
+                  <FormControl
+                    {...register("address", { required: true })}
+                    className="input-field mb-3"
+                    placeholder="Address"
+                    aria-label="address"
+                    aria-describedby="basic-addon2"
+                  />
 
-                <Form.Group className="mb-3" controlId="formBasicCheckbox">
-                  <Form.Text className="text-danger"></Form.Text>
-                </Form.Group>
-                <Form.Group
-                  className="mb-3"
-                  controlId="exampleForm.ControlTextarea1"
-                ></Form.Group>
-                {errors.exampleRequired && <span>This field is required</span>}
-                <div className="d-grid">
-                  <Button
-                    variant="dark"
-                    type="submit"
-                    className="btn common-btn3 py-3"
-                  >
-                    Order Now
-                  </Button>
-                </div>
-              </Form>
-            </Col>
-          </Row>
+                  <Form.Group className="mb-3" controlId="formBasicCheckbox">
+                    <Form.Text className="text-danger"></Form.Text>
+                  </Form.Group>
+                  <Form.Group
+                    className="mb-3"
+                    controlId="exampleForm.ControlTextarea1"
+                  ></Form.Group>
+                  {errors.exampleRequired && (
+                    <span>This field is required</span>
+                  )}
+                  <div className="d-grid">
+                    <Button
+                      variant="dark"
+                      type="submit"
+                      className="btn common-btn3 py-3"
+                    >
+                      Order Now
+                    </Button>
+                  </div>
+                </Form>
+              </Col>
+            </Row>
 
-          {/* <Button
+            {/* <Button
             // onClick={() => handleBook(selectedService?._id)}
             variant="light"
             className="common-btn3 mt-4"
@@ -151,18 +161,20 @@ const PropertyDetail = () => {
             Book Now
           </Button> */}
 
-          <Link to="/services">
-            <Button variant="light" className=" mt-4 ms-3">
-              Back
-            </Button>
-          </Link>
-          <Link to="/myorder">
-            <Button variant="light" className=" mt-4 ms-3">
-              See all of your orders
-            </Button>
-          </Link>
-        </Col>
-      </Row>
+            <Link to="/services">
+              <Button variant="light" className=" mt-4 ms-3">
+                Back
+              </Button>
+            </Link>
+            <Link to="/myorder">
+              <Button variant="light" className=" mt-4 ms-3">
+                See all of your orders
+              </Button>
+            </Link>
+          </Col>
+        </Row>
+      </div>
+      <Footer />
     </div>
   );
 };
